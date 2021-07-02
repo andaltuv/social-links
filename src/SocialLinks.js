4cf8dcf886efb17a996949895bccf824c8613008
andaltuv <andresaltuve@gmail.com>
2021-07-02 11:53:22 -0400

    Added css breakpoints, Google Analytics, Footer

--- src/SocialLinks.js
+++ src/SocialLinks.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import { Footer } from './components/Footer/Footer';
 import { LinksList } from './components/Links/LinksList';
 
 export const SocialLinks = () => {
 
     return(
-        <>
-              
+        <>        
         <LinksList/>
+        <Footer />
         </>        
     )
 
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Footer = () => {
+
+    return(
+        <footer>
+            <a href="https://github.com/andaltuv/social-links">Social Links</a>
+        </footer>
+    )
+
+}
